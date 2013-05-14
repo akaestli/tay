8387dfa92c21209d74462ce0d7a43339aa96017c
akaestli <a.kaestli@gmail.com>
2013-05-14 10:54:41 +0200

    Commit message

---
+++ tay/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TayGroup::Application.config.secret_token = '40aae07c3db9f473f90739c069d80226867b2d0c913509d51be33696031a94cad8ae092efab541a0c893359669d2f595ffed81f586da7533031fc8721d249d7a'
