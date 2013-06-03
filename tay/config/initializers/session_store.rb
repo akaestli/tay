b9b435efd7db0a113b33f9fd8bc338dfaa91e0a8
akaestli <a.kaestli@gmail.com>
2013-06-03 15:49:27 +0200

    delete tay folder

--- tay/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-TayGroup::Application.config.session_store :cookie_store, key: '_Tay_Group_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# TayGroup::Application.config.session_store :active_record_store
