7555940a6144f00cb706abf8d839dd4984dd2a1d
akaestli <a.kaestli@gmail.com>
2013-06-03 16:46:40 +0200

    updated deply.rb

--- Tay/config/deploy.rb
+++ Tay/config/deploy.rb
@@ -6,17 +6,14 @@ set :applicationdir, "/local0/www/html/Tay"
  
 set :scm, 'git'
 set :repository,  "git://github.com/akaestli/tay.git"
-# changed on jun, 03, 2013
-# set :git_enable_submodules, 1 # if you have vendored rails
 set :branch, 'master'
-set :git_shallow_clone, 1
-set :scm_verbose, true
-set :use_sudo, true
+#set :scm_verbose, true
+#set :use_sudo, true
+set :use_sudo, false
 
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
 
 # deploy config - added by alicia on may 15
 set :deploy_to, applicationdir
@@ -32,10 +29,25 @@ default_run_options[:pty] = true
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
+#namespace :deploy do
+#  task :start do ; end
+#  task :stop do ; end
+#  task :restart, :roles => :app, :except => { :no_release => true } do
+#    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#  end
+#end
+
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
 end
