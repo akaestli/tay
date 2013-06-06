a2bc877ed92afe5df7be634f9727bc348c8a8da4
akaestli <a.kaestli@gmail.com>
2013-06-06 15:15:58 +0200

    deploy.rb update

--- Tay/config/deploy.rb
+++ Tay/config/deploy.rb
@@ -1,14 +1,15 @@
 require 'bundler/capistrano'
-set :user, "kaestlal"
+set :application, "Tay"
+#set :user, "kaestlal"
 set :domain, "microfluidics.ethz.ch"
-set :application, "microfluidics.ethz.ch"
-set :applicationdir, "/local0/www/html/Tay"
+#set :applicationdir, "/local0/www/html/Tay"
+set :deploy_to, "/local0/www/html/#{application}"
  
 default_run_options[:pty] = true 
 set :scm, 'git'
 set :repository,  "git://github.com/akaestli/tay.git"
 set :branch, 'master'
-set :scm_verbose, true
+#set :scm_verbose, true
 set :use_sudo, true
 
 role :web, domain                          # Your HTTP server, Apache/etc
@@ -16,8 +17,8 @@ role :app, domain                          # This may be the same as your `Web` 
 role :db,  domain, :primary => true # This is where Rails migrations will run
 
 # deploy config - added by alicia on may 15
-set :deploy_to, applicationdir
-set :deploy_via, :export
+#set :deploy_to, applicationdir
+#set :deploy_via, :export
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -26,10 +27,25 @@ set :deploy_via, :export
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
+#namespace :deploy do
+#  task :start do ; end
+#  task :stop do ; end
+#  task :restart, :roles => :app, :except => { :no_release => true } do
+#    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#  end
+#end
+
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
 end
