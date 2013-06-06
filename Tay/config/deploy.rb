abb09ebdc1479b8a506888edabd8efcc4bbd5715
akaestli <a.kaestli@gmail.com>
2013-06-06 15:06:24 +0200

    deploy.rb edited

--- Tay/config/deploy.rb
+++ Tay/config/deploy.rb
@@ -4,11 +4,12 @@ set :domain, "microfluidics.ethz.ch"
 set :application, "microfluidics.ethz.ch"
 set :applicationdir, "/local0/www/html/Tay"
  
+default_run_options[:pty] = true 
 set :scm, 'git'
 set :repository,  "git://github.com/akaestli/tay.git"
 set :branch, 'master'
 set :scm_verbose, true
-set :use_sudo, true
+set :use_sudo, false #changed from true on jun 06
 
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
@@ -17,9 +18,6 @@ role :db,  domain, :primary => true # This is where Rails migrations will run
 # deploy config - added by alicia on may 15
 set :deploy_to, applicationdir
 set :deploy_via, :export
-
-# Added by alicia on may 16 2013
-default_run_options[:pty] = true
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
