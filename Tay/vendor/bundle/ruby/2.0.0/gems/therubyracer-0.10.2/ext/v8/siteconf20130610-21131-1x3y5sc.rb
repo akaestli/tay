e0073c95a3db21eae33f4d1675de94740a132bd0
akaestli <a.kaestli@gmail.com>
2013-06-10 12:28:45 +0200

    website finally back up :)

---
+++ Tay/vendor/bundle/ruby/2.0.0/gems/therubyracer-0.10.2/ext/v8/siteconf20130610-21131-1x3y5sc.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130610-21131-s19kzn"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
